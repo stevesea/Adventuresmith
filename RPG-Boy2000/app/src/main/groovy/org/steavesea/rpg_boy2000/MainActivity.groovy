4c9fae061b104c0010fdcc3096e76a2c8a1829e1
Stephen Christensen <steve.christensen@sandisk.com>
2016-09-25 15:29:25 -0600

    begin the conversion

--- RPG-Boy2000/app/src/main/groovy/org/steavesea/rpg_boy2000/MainActivity.groovy
+++ RPG-Boy2000/app/src/main/groovy/org/steavesea/rpg_boy2000/MainActivity.groovy
@@ -1,20 +1,20 @@
-package org.steavesea.rpg_boy2000;
+package org.steavesea.rpg_boy2000
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.arasthel.swissknife.SwissKnife
 import com.arasthel.swissknife.annotations.InjectView
-import groovy.transform.CompileStatic;
+import com.arasthel.swissknife.annotations.OnClick
+import groovy.transform.CompileStatic
 
 @CompileStatic
 public class MainActivity extends AppCompatActivity
@@ -22,12 +22,17 @@ public class MainActivity extends AppCompatActivity
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar
-    @InjectView(R.id.fab)
-    FloatingActionButton floatingActionButton
     @InjectView(R.id.drawer_layout)
     DrawerLayout drawer;
     @InjectView(R.id.nav_view)
     NavigationView navigationView;
+
+    @OnClick(R.id.fab)
+    public void onClickFloater(View v) {
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +51,6 @@ public class MainActivity extends AppCompatActivity
         SwissKnife.loadExtras(this)
 
         setSupportActionBar(toolbar);
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -101,13 +98,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_fotf) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mr) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_pw) {
 
         } else if (id == R.id.nav_share) {
 
@@ -118,4 +113,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
