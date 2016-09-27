27c825b6e4bc7b9abfa031efc1641eda969cbb3d
Stephen Christensen <steve.christensen@sandisk.com>
2016-09-27 10:46:09 -0600

    add copyright headers

--- RPG-Boy2000/app/src/main/groovy/org/steavesea/rpg_boy2000/RpgBoy2000Module.groovy
+++ RPG-Boy2000/app/src/main/groovy/org/steavesea/rpg_boy2000/RpgBoy2000Module.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016 Steve Christensen
+ *
+ * This file is part of RPG-Boy 2000.
+ *
+ * RPG-Boy 2000 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RPG-Boy 2000 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RPG-Boy 2000.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.steavesea.rpg_boy2000
 
 import dagger.Module
