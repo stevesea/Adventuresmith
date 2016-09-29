5a3eae3ed5f22b9baff266b05aa740c5f4329f30
Stephen Christensen <steve.christensen@sandisk.com>
2016-09-29 17:57:03 -0600

    update name in copyright notice

--- app/src/main/groovy/org/stevesea/rpgpad/data/RpgPadDataModule.groovy
+++ app/src/main/groovy/org/stevesea/rpgpad/data/RpgPadDataModule.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016 Steve Christensen
+ *
+ * This file is part of RPG-Pad.
+ *
+ * RPG-Pad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RPG-Pad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RPG-Pad.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.stevesea.rpgpad.data
 
 import dagger.Module
--- app/src/test/groovy/org/stevesea/rpgpad/data/MazeRatsGeneratorTest.groovy
+++ app/src/test/groovy/org/stevesea/rpgpad/data/MazeRatsGeneratorTest.groovy
@@ -1,22 +1,21 @@
 /*
  * Copyright (c) 2016 Steve Christensen
  *
- * This file is part of RPG-Boy 2000.
+ * This file is part of RPG-Pad.
  *
- * RPG-Boy 2000 is free software: you can redistribute it and/or modify
+ * RPG-Pad is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * RPG-Boy 2000 is distributed in the hope that it will be useful,
+ * RPG-Pad is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with RPG-Boy 2000.  If not, see <http://www.gnu.org/licenses/>.
+ * along with RPG-Pad.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.stevesea.rpgpad.data
 
 import groovy.transform.CompileStatic
