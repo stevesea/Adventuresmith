005f418725f6fe8c99ba5d7eb490ad3b5aa2e37c
Stephen Christensen <steve.christensen@sandisk.com>
2016-09-29 18:00:54 -0600

    one last rename boy -> pad

--- app/src/test/groovy/org/stevesea/rpgpad/RpgPadDataTest.groovy
+++ app/src/test/groovy/org/stevesea/rpgpad/RpgPadDataTest.groovy
@@ -1,24 +1,24 @@
-package org.stevesea.rpgpad.data
+package org.stevesea.rpgpad
 
 import groovy.transform.CompileStatic
 import org.junit.Assert
 import org.junit.Test
-import org.stevesea.rpgpad.Dataset
-import org.stevesea.rpgpad.DatasetButton
 
 @CompileStatic
-class RpgBoyDataTest {
+class RpgPadDataTest {
 
     @Test
     void verifyButtonsMr() {
         Assert.assertEquals([DatasetButton.MrCharacters, DatasetButton.MrMonsters, DatasetButton.MrMagic, DatasetButton.MrItems, DatasetButton.MrAfflictions, DatasetButton.MrPotionEffects],
                 DatasetButton.getButtonsForDataset(Dataset.MazeRats))
     }
+
     @Test
     void verifyButtonsPw() {
         Assert.assertEquals([DatasetButton.PerilousPlaces, DatasetButton.PerilousRegions],
                 DatasetButton.getButtonsForDataset(Dataset.ThePerilousWilds))
     }
+    
     @Test
     void verifyButtonsFotF() {
         Assert.assertEquals([DatasetButton.FreebooterSpells, DatasetButton.FreebooterTraits],
