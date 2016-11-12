d62fbbfccfffa27b5c3195bd432a679f7052052b
Stephen Christensen <stevec1973@gmail.com>
2016-11-12 01:01:30 -0700

    get rid of obsolete tests

--- app/src/test/groovy/org/stevesea/adventuresmith/data_k/DataDrivenTest.groovy
+++ app/src/test/groovy/org/stevesea/adventuresmith/data_k/DataDrivenTest.groovy
@@ -23,7 +23,6 @@ package org.stevesea.adventuresmith.data_k
 import android.content.Context
 import android.os.Build
 import groovy.transform.CompileStatic
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,11 +32,6 @@ import org.robolectric.annotation.Config
 import org.stevesea.adventuresmith.AdventuresmithApp
 import org.stevesea.adventuresmith.BuildConfig
 import org.stevesea.adventuresmith.core.Shuffler
-import org.stevesea.adventuresmith.core.freebooters_on_the_frontier.FotfSpellGenerator
-import org.stevesea.adventuresmith.core.fourth_page.FourthPageArtifactGenerator
-import org.stevesea.adventuresmith.core.fourth_page.FourthPageCityGenerator
-import org.stevesea.adventuresmith.core.fourth_page.FourthPageDungeonGenerator
-import org.stevesea.adventuresmith.core.fourth_page.FourthPageMonsterGenerator
 
 import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.when
@@ -59,201 +53,15 @@ class DataDrivenTest {
     @Before
     void setup() {
         context = RuntimeEnvironment.application
-        ContextProvider.context = context
+        //ContextProvider.context = context
         def mockRandom = mock(Random.class)
         when(mockRandom.nextInt()).thenReturn(1)
-        shuffler = new Shuffler(mockRandom)
+        //shuffler = new Shuffler(mockRandom)
 
     }
 
     @Test
     void testFpArtifact() {
-        Assert.assertEquals("""\
-<html>
-  <body>
-    <h4>
-      Artifact
-    </h4>
-    <h5>
-      Origin
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Holy
-        </small>
-      </strong>
-      - It's been lost for centuries.
-    </p>
-    <h5>
-      Power
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Blessing
-        </small>
-      </strong>
-      - You see visions of the future.
-    </p>
-  </body>
-</html>
-""", new FourthPageArtifactGenerator(shuffler).generate(Locale.ENGLISH))
-    }
 
-    @Test
-    void testFpDungeon() {
-        Assert.assertEquals("""\
-<html>
-  <body>
-    <h4>
-      Dungeon
-    </h4>
-    <h5>
-      History
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Builder
-        </small>
-      </strong>
-      - The walls show signs of dwarven craftsmanship.
-    </p>
-    <h5>
-      Denizen
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Community
-        </small>
-      </strong>
-      - It's a perfect hideout for the outlaws.
-    </p>
-    <h5>
-      Trial
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Dilemma
-        </small>
-      </strong>
-      - She's just a child.
-    </p>
-    <h5>
-      Secret
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Cache
-        </small>
-      </strong>
-      - The library is full of ancient texts.
-    </p>
-  </body>
-</html>
-""", new FourthPageDungeonGenerator(shuffler).generate(Locale.ENGLISH))
-    }
-
-    @Test
-    void testFpCity() {
-        Assert.assertEquals("""\
-<html>
-  <body>
-    <h4>
-      City
-    </h4>
-    <h5>
-      Feature
-    </h5>
-    <p>
-      <strong>
-        <small>
-          District
-        </small>
-      </strong>
-      - The garrison is a well-oiled machine.
-    </p>
-    <h5>
-      Population
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Attitude
-        </small>
-      </strong>
-      - They want you gone as soon as possible.
-    </p>
-    <h5>
-      Society
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Faith
-        </small>
-      </strong>
-      - Your heathen ways will not be tolerated.
-    </p>
-    <h5>
-      Trouble
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Rumour
-        </small>
-      </strong>
-      - There's treasure in those caves!
-    </p>
-  </body>
-</html>
-""",  new FourthPageCityGenerator(shuffler).generate(Locale.ENGLISH))
-
-    }
-
-    @Test
-    void testFpMonster() {
-        Assert.assertEquals("""\
-<html>
-  <body>
-    <h4>
-      Monster
-    </h4>
-    <h5>
-      Nature
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Animal and Plant
-        </small>
-      </strong>
-      - Its barbs are filled with poison.
-    </p>
-    <h5>
-      Role
-    </h5>
-    <p>
-      <strong>
-        <small>
-          Artillery
-        </small>
-      </strong>
-      - They shoot at you from cover.
-    </p>
-  </body>
-</html>
-""",  new FourthPageMonsterGenerator(shuffler).generate(Locale.ENGLISH))
-
-    }
-
-    @Test
-    void testFotfSpell() {
-        Assert.assertEquals("Acid Arrow",  new FotfSpellGenerator(shuffler).generate(Locale.ENGLISH))
     }
 }
