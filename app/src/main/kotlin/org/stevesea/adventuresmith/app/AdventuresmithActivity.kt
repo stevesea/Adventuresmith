6d193f80ead38dac9d5e5cd14f93b1676a87e605
Stephen Christensen <stevec1973@gmail.com>
2016-11-23 14:35:13 -0700

    more SWN generators -- mandate tech, scavenger fleet

--- app/src/main/kotlin/org/stevesea/adventuresmith/app/AdventuresmithActivity.kt
+++ app/src/main/kotlin/org/stevesea/adventuresmith/app/AdventuresmithActivity.kt
@@ -454,6 +454,7 @@ class AdventuresmithActivity : AppCompatActivity(),
                         "grp1" -> Ionicons.Icon.ion_planet // aliens, animals, worlds
                         "grp2" -> Ionicons.Icon.ion_person_stalker // npcs, corps, religions
                         "grp3" -> Ionicons.Icon.ion_edit // names
+                        "grp4" -> CommunityMaterial.Icon.cmd_factory // tech, architecture
                         else -> CommunityMaterial.Icon.cmd_rocket
                     }
                 }
