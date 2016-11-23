42559ee193edbf41c0c7f96b5bdfc7623d8e82eb
Stephen Christensen <stevec1973@gmail.com>
2016-11-22 18:22:50 -0700

    put version # with app info

--- app/src/main/kotlin/org/stevesea/adventuresmith/app/AboutActivity.kt
+++ app/src/main/kotlin/org/stevesea/adventuresmith/app/AboutActivity.kt
@@ -69,8 +69,7 @@ class AboutActivity : AppCompatActivity() {
         recycler_about.itemAnimator = DefaultItemAnimator()
         recycler_about.adapter = itemAdapter
 
-        itemAdapter.add(ResultItem(String.format(getString(R.string.about_version), versionName, versionCode)))
-        itemAdapter.add(ResultItem(getString(R.string.about_app)))
+        itemAdapter.add(ResultItem(String.format(getString(R.string.about_app), versionName, versionCode)))
         itemAdapter.add(ResultItem(getString(R.string.about_thirdparty)))
     }
 }
